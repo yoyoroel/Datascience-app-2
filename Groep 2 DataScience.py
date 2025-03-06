bd4206afab944d019a9dcdc9e10e672090163302
yoyoroel <roel.van.den.boer@hva.nl>
2025-03-06 15:23:42 +0100

    Update Groep 2 DataScience.py

--- Groep 2 DataScience.py
+++ Groep 2 DataScience.py
@@ -477,7 +477,7 @@ def per_land():
     # Maak de bar chart
     fig = px.bar(
         top_3_videos,
-        x='Top_3',
+        x='rank_position',
         y='height',
         labels={'height': 'Ranking van Video'},
         title=f"Top 3 Video's in {Inputland}",
