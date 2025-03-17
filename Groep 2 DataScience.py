86fbcc31865914fea697eb1afb6c26c073305789
yoyoroel <roel.van.den.boer@hva.nl>
2025-03-17 15:13:05 +0100

    Update Groep 2 DataScience.py

--- Groep 2 DataScience.py
+++ Groep 2 DataScience.py
@@ -1,21 +1,40 @@
-#!pip install folium
+#!pip install folium streamlit streamlit_folium
 import pandas as pd
 import numpy as np
 import streamlit as st
 import plotly.express as px
 import folium
+from streamlit_folium import folium_static
+
 pd.set_option('display.max_columns', None)
 
-#data import
-flightdata1 = pd.read_excel('case3/30Flight 1.xlsx')
-flightdata2 = pd.read_excel('case3/30Flight 2.xlsx')
-flightdata3 = pd.read_excel('case3/30Flight 3.xlsx')
-flightdata4 = pd.read_excel('case3/30Flight 4.xlsx')
-flightdata5 = pd.read_excel('case3/30Flight 5.xlsx')
-flightdata6 = pd.read_excel('case3/30Flight 6.xlsx')
-flightdata7 = pd.read_excel('case3/30Flight 7.xlsx')
+# Data import
+flightdata1 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 1.xlsx')
+flightdata2 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 2.xlsx')
+flightdata3 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 3.xlsx')
+flightdata4 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 4.xlsx')
+flightdata5 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 5.xlsx')
+flightdata6 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 6.xlsx')
+flightdata7 = pd.read_excel('/Users/Roel/Library/CloudStorage/OneDrive-HvA/School/Data Science/week 5/case 3 vluchten/case3/30Flight 7.xlsx')
 
-#kleur voor de hoogte
+# Combine all flight data into a dictionary
+flightdata_options = {
+    'Flight 1': flightdata1,
+    'Flight 2': flightdata2,
+    'Flight 3': flightdata3,
+    'Flight 4': flightdata4,
+    'Flight 5': flightdata5,
+    'Flight 6': flightdata6,
+    'Flight 7': flightdata7
+}
+
+# Create a select box for flight options
+selected_flight = st.selectbox('Select a flight', list(flightdata_options.keys()))
+
+# Get the selected flight data
+flightdata = flightdata_options[selected_flight]
+
+# kleur voor de hoogte
 def flkleur(hoogte):
     if hoogte > 40000:
         return 'purple'
@@ -31,22 +50,6 @@ def flkleur(hoogte):
         return 'yellow'
     elif hoogte > 2000:
         return 'gold'
-    else:
-        return 'red'
-
-# Select flight data
-flightdata_options = {
-    'Flight 1': flightdata1,
-    'Flight 2': flightdata2,
-    'Flight 3': flightdata3,
-    'Flight 4': flightdata4,
-    'Flight 5': flightdata5,
-    'Flight 6': flightdata6,
-    'Flight 7': flightdata7
-}
-
-selected_flight = st.selectbox('Select Flight Data', list(flightdata_options.keys()))
-flightdata = flightdata_options[selected_flight]
 
 # Create map for selected flight
 m = folium.Map(location=[52.303928, 4.765694], zoom_start=5)
@@ -62,4 +65,5 @@ for i in flightdata.index:
         radius=500
     ).add_to(m)
 
-m
+# Display the map
+folium_static(m, width=700, height=500)
